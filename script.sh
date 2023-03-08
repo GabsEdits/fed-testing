2bc42101d0664916784be3a16d58e0717b169ba6
gabs <110247388+GabsEdits@users.noreply.github.com>
2023-03-08 13:53:00 +0200

    Add APX

--- script.sh
+++ script.sh
@@ -8,52 +8,59 @@ usage() {
   echo "  -a  Install apps"
   echo "  -t  Set up adw-gtk3 & dark sytle"
   echo "  -p  Add Flathub"
-  echo "  -x  Add apx"
+  echo "  -x  Add Apx"
   exit 1
 }
 
-while getopts ":frmatp" option; do
+while getopts ":frmatpx" option; do
   case "${option}" in
     f)
-if ! grep -q "^fastestmirror=True" /etc/dnf/dnf.conf; then
-  sh -c 'echo "fastestmirror=True" >> /etc/dnf/dnf.conf'
-fi
+      # Make dnf faster
+      if ! grep -q "^fastestmirror=True" /etc/dnf/dnf.conf; then
+        sh -c 'echo "fastestmirror=True" >> /etc/dnf/dnf.conf'
+      fi
 
-if ! grep -q "^max_parallel_downloads=10" /etc/dnf/dnf.conf; then
-  sh -c 'echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf'
-fi
+      if ! grep -q "^max_parallel_downloads=10" /etc/dnf/dnf.conf; then
+        sh -c 'echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf'
+      fi
 
-if ! grep -q "^defaultyes=True" /etc/dnf/dnf.conf; then
-  sh -c 'echo "defaultyes=True" >> /etc/dnf/dnf.conf'
-fi
+      if ! grep -q "^defaultyes=True" /etc/dnf/dnf.conf; then
+        sh -c 'echo "defaultyes=True" >> /etc/dnf/dnf.conf'
+      fi
 
-if ! grep -q "^keepcache=True" /etc/dnf/dnf.conf; then
-  sh -c 'echo "keepcache=True" >> /etc/dnf/dnf.conf'
-fi
+      if ! grep -q "^keepcache=True" /etc/dnf/dnf.conf; then
+        sh -c 'echo "keepcache=True" >> /etc/dnf/dnf.conf'
+      fi
       ;;
     r)
+      # Add RPM Fusion
       dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
       ;;
     m)
+      # Add Multimedia Codecs
       dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
       dnf groupupdate sound-and-video -y
       ;;
     a)
+      # Install apps
       dnf install discord -y
       wget https://download.cdn.viber.com/desktop/Linux/viber.rpm && rpm -i viber.rpm
       flatpak install flathub com.jetbrains.IntelliJ-IDEA-Community -y
       ;;
     t)
+      # Set up adw-gtk3 & dark style
       dnf copr enable nickavem/adw-gtk3 -y
       dnf install adw-gtk3 -y
       gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
       ;;
     p)
+      # Add Flathub
       flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
       flatpak --user override --filesystem=/home/$USER/.icons/:ro
       flatpak --user override --filesystem=/usr/share/icons/:ro
       ;;
-      x)
+    x)
+      # Add Apx
       dnf install go -y
       curl -s https://raw.githubusercontent.com/spaceguybob/apx-install/main/installer.sh | sh
       ;;
@@ -62,9 +69,3 @@ fi
       ;;
   esac
 done
-
-# Check if Script is Run as Root
-if [[ $EUID -ne 0 ]]; then
-  echo "You must be a root user to run this script, please run sudo sh ./script.sh" 2>&1
-  exit 1
-fi
